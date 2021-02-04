12032347f7a02db855d66076650b105b8d5a6679
paddymills <paddymills@pm.me>
2021-02-04 09:55:54 +0000

    initial commit

---
+++ src/WordSearch.java
@@ -0,0 +1,22 @@
+
+/**
+    Author:             Patrick Miller
+    E-mail:             pjm6196@psu.edu
+    Course:             CMPSC 221
+    Assignment:         Project 1
+    Due date:           1/5/2021
+    File:               WordSearch.java
+    Purpose:            Word search game to demonstrate string methods
+    Compiler/IDE:       openjdk-15/VisualStudioCode
+    Operating system:   Linux pop-os 5.8.0
+    Reference(s):       OpenJDK 15 (devdocs.io)
+                        (https://devdocs.io/openjdk~15/);
+*/
+
+public class WordSearch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
