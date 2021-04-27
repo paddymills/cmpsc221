7deba72fa585d75716c0055ab981ffc3d5ab7165
paddymills <paddymills@pm.me>
2021-04-27 15:13:04 +0000

    init project 5

---
+++ src/MusicDb/MusicDb.java
@@ -0,0 +1,21 @@
+package MusicDb;
+
+/**
+    Author:             Patrick Miller
+    E-mail:             pjm6196@psu.edu
+    Course:             CMPSC 221
+    Assignment:         Project 2
+    Due date:           5/5/2021
+    File:               MusicDb.java
+    Purpose:            Music Database driver
+    Compiler/IDE:       openjdk-15/VisualStudioCode
+    Operating system:   Linux pop-os 5.11.0
+    Reference(s):       OpenJDK 15 (devdocs.io) (https://devdocs.io/openjdk~15/)
+*/
+
+public class MusicDb {
+
+    public static void main(String[] args) {
+        
+    }
+}
